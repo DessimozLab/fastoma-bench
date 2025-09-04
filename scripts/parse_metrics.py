a2c6d46d4ecdc2567b468c941bc16fc8f1d5ea74
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2025-09-04 14:31:56 +0200

    extract metrics for subhog assertions

--- scripts/parse_metrics.py
+++ scripts/parse_metrics.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
+
+import itertools
 import re
 import pathlib
 from collections import defaultdict
-from typing import Dict, Any
+from typing import Dict, Any, Set
 
 import yaml
 import pyham
+from pyham.abstractgene import HOG
 
 
 def parse_elapsed(s: str | None) -> float | None:
@@ -54,7 +57,6 @@ def check_subhog_assertions(ham: pyham.Ham, assert_file: pathlib.Path) -> Dict[s
     """
     metrics = {
         "n_groups": 0,
-        "n_subgroups": 0,
         "n_assertions": 0,
         "n_passed": 0,
         "n_failed": 0,
@@ -62,6 +64,55 @@ def check_subhog_assertions(ham: pyham.Ham, assert_file: pathlib.Path) -> Dict[s
         "overlaps": []
     }
     assertions = yaml.safe_load(assert_file.read_text())
+
+    def _get_groups_for_reference_geneset(grp: set[str], complete: bool = False) -> Dict[HOG, Set[str]]:
+        found = defaultdict(set)
+        for m in grp:
+            try:
+                gene = ham.get_genes_by_external_id(m)[0]
+                group, by_dup = gene.search_ancestor_hog_in_ancestral_genome(anc_genome)
+                if group is not None:
+                    found[group].add(m)
+            except KeyError:
+                pass
+        if complete:
+            for group in found:
+                all_grp_members = group.get_all_descendant_genes()
+                found[group].update((x.prot_id for x in all_grp_members))
+        return found
+
+    def _compare_group_sets(expected: set[str], found: Dict[HOG, Set[str]], name: str|None = None) -> None:
+        """ Compare expected and found group sets.
+
+        expected is always a single set with protein IDs. The found one can be split into multiple groups.
+
+        The function updates metrics dict with pass/fail counts and violations."""
+        if name is None:
+            name = "main"
+        if len(found) == 1:
+            actual = found.popitem()[1]
+            if actual == expected:
+                metrics["n_passed"] += 1
+            else:
+                metrics["n_failed"] += 1
+                missing = expected - actual
+                extra = actual - expected
+                metrics["violations"].append(
+                    {"level": level, "name": name, "error": f"Protein Sets are not identical: missing {sorted(missing)}; extra {sorted(extra)}"}
+                )
+            metrics["overlaps"].append({"level": level, "name": name,  "venn": (len(expected-actual), len(actual & expected), len(actual-expected))})
+        else:
+            metrics["n_failed"] += 1
+            metrics["violations"].append(
+                {"level": level, "name": name,
+                 "error": f"Split into {len(found)} groups: {len(expected)} members, found {sum(len(z) for z in found.values())}"}
+            )
+            metrics["overlaps"].append(
+                {"level": level, "name": name, "venn":
+                   (len(expected),
+                    max([0, *(len(z & expected) for z in found.values())]),
+                    len(set.union(*found.values()) - expected))
+                })
 
     for grp in assertions.get('groups', []):
         metrics["n_groups"] += 1
@@ -76,36 +127,29 @@ def check_subhog_assertions(ham: pyham.Ham, assert_file: pathlib.Path) -> Dict[s
 
         if "members" in grp:
             expected = set(grp["members"])
-            found = defaultdict(set)
-            for m in expected:
-                try:
-                    gene = ham.get_genes_by_external_id(m)[0]
-                    group, by_dup = gene.search_ancestor_hog_in_ancestral_genome(anc_genome)
-                    if group is not None:
-                        found[group].add(m)
-                except KeyError:
-                    pass
-            if grp.get("complete", False):
-                for group in found:
-                    all_grp_members = group.get_all_descendant_genes()
-                    found[group].update((x.prot_id for x in all_grp_members))
-            if len(found) == 1:
-                actual = found.popitem()[1]
-                if actual == expected:
-                    metrics["n_passed"] += 1
-                else:
-                    metrics["n_failed"] += 1
-                    metrics["violations"].append(
-                        {"group": level, "expected": sorted(expected), "actual": sorted(actual)}
-                    )
-                metrics["overlaps"].append({level: (len(expected), len(actual & expected))})
-            else:
-                metrics["n_failed"] += 1
-                metrics["violations"].append(
-                    {"group": level, "error": f"Split into {len(found)} groups: {len(expected)} members, found {sum(len(z) for z in found.values())}"}
-                )
-                metrics["overlaps"].append({level: (len(expected), max([0, *(len(z & expected) for z in found.values())]))})
+            found = _get_groups_for_reference_geneset(expected, grp.get("complete", False))
+            _compare_group_sets(expected, found)
         # TODO: same for subgroups
+        elif "subgroups" in grp:
+            subgroups = grp["subgroups"]
+            groups_per_subgroups = []
+            for i, subgrp in enumerate(subgroups, start=1):
+                subgrp["members"] = set(subgrp["members"])
+                found = _get_groups_for_reference_geneset(subgrp["members"], subgrp.get("complete", False))
+                _compare_group_sets(subgrp["members"], found, subgrp.get("name", f"subgroup_{i}"))
+                groups_per_subgroups.append(found)
+            # check for overlaps between subgroups
+            for i, j in itertools.combinations(range(len(subgroups)), 2):
+                grs1 = groups_per_subgroups[i]
+                grs2 = groups_per_subgroups[j]
+                if len(set(grs1.keys()) & set(grs2.keys())) > 0:
+                    metrics["n_failed"] += 1
+                    metrics["violations"].append({
+                        "level": level, "name": "subgroup_overlap",
+                        "error": f'Subgroups overlap between {subgroups[i].get("name", f"subgroup_{i}")} and '
+                                 f'{subgroups[j].get("name", f"subgroup_{j}")}: '
+                                 f'{len(set(grs1.keys()) & set(grs2.keys()))} common HOGs'
+                    })
     return metrics
 
 
